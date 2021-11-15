698df34180d8427b14c59361a57f56d710154c58
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-11-15 23:00:11 +0900

    update

--- src/electron-ipc-cat/common.ts
+++ src/electron-ipc-cat/common.ts
@@ -1,4 +1,4 @@
-import { Asyncify, ConditionalKeys } from 'type-fest'
+import { ConditionalKeys } from 'type-fest'
 import { Observable } from 'rxjs'
 
 export type ProxyAsyncProperties<OriginalProxy> = ConditionalKeys<OriginalProxy, (..._arguments: never) => any>
@@ -8,22 +8,22 @@ export type ProxyObservableProperties<OriginalProxy> =
 export type ProxyWithOnlyObservable<OriginalProxy> = Pick<OriginalProxy, ProxyObservableProperties<OriginalProxy>>
 export type ProxyWithOutObservable<OriginalProxy> = Omit<OriginalProxy, ProxyObservableProperties<OriginalProxy>>
 
-/**
- * To call services that is located in main process, from the renderer process, we use IPC.invoke, so all method should now promisify
- * Note this type only promisify methods that return things, not methods that returns observable.
- */
-export type AsyncifyProxy<
-    OriginalProxy,
-    ObservableKey extends ProxyObservableProperties<OriginalProxy> = ProxyObservableProperties<OriginalProxy>,
-    AsyncKey extends Exclude<ProxyAsyncProperties<OriginalProxy>, ObservableKey> = Exclude<
-        ProxyAsyncProperties<OriginalProxy>,
-        ObservableKey
-    >,
-> = {
-    [P in AsyncKey]: Asyncify<OriginalProxy[P]>
-} & {
-    [Q in ObservableKey]: OriginalProxy[Q]
-}
+// /**
+//  * To call services that is located in main process, from the renderer process, we use IPC.invoke, so all method should now promisify
+//  * Note this type only promisify methods that return things, not methods that returns observable.
+//  */
+// export type AsyncifyProxy<
+//     OriginalProxy,
+//     ObservableKey extends ProxyObservableProperties<OriginalProxy> = ProxyObservableProperties<OriginalProxy>,
+//     AsyncKey extends Exclude<ProxyAsyncProperties<OriginalProxy>, ObservableKey> = Exclude<
+//         ProxyAsyncProperties<OriginalProxy>,
+//         ObservableKey
+//     >,
+// > = {
+//     [P in AsyncKey]: Asyncify<OriginalProxy[P]>
+// } & {
+//     [Q in ObservableKey]: OriginalProxy[Q]
+// }
 
 /** Extract observable keys from services */
 export type IServicesWithOnlyObservables<Services> = {
