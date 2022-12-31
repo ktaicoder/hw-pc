b372d6adf35357ffde531704a1ccba3b0636ed44
unknown <zzuooooo@gmail.com>
2022-12-31 21:31:21 +0900

    exMars-Cube 추가

---
+++ src/custom/hw/exMarsCube/index.ts
@@ -0,0 +1,26 @@
+import { HwKind, IHwInfo } from 'src/custom-types/hw-types';
+import { ExMarsCubeControl } from './ExMarsCubeControl';
+
+const HWID = 'exMarsCube';
+
+const info: IHwInfo = {
+    hwId: HWID,
+    hwKind: HwKind.serial,
+    hwName: 'eX-Mars Cube',
+    category: 'robot',
+    supportPlatforms: ['win32'],
+    pcDrivers: [
+        {
+            name: 'USB 드라이버',
+            'win32-ia32': 'CH34x/CH34x_Install_Windows_v3_4.exe',
+            'win32-x64': 'CH34x/CH34x_Install_Windows_v3_4.exe',
+        },
+    ],
+}
+
+export default {
+    hwId: HWID,
+    info,
+    operator: ExMarsCubeControl,
+    control: () => new ExMarsCubeControl(),
+}
